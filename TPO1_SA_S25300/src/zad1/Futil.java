e71f489510eacc74868b56143e7cfb14343ebf58
adamsiedlecki <adamsiedlec@gmail.com>
2024-03-18 22:12:49 +0100

    java 8 method

--- TPO1_SA_S25300/src/zad1/Futil.java
+++ TPO1_SA_S25300/src/zad1/Futil.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Futil{
   public static void processDir(String dirName, String resultFileName) {
@@ -19,7 +19,7 @@ public class Futil{
   private static void process(String dirName, String resultFileName) throws IOException {
     try(RandomAccessFile randomAccessFileOutput = new RandomAccessFile(resultFileName, "rw")) {
       FileChannel fileOutputChannel = randomAccessFileOutput.getChannel();
-      Files.walkFileTree(Path.of(dirName), new MyFileVisitor(fileOutputChannel));
+      Files.walkFileTree(Paths.get(dirName), new MyFileVisitor(fileOutputChannel));
     }
   }
 }
